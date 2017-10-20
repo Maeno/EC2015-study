14e76f389b6e8d1dc629981f8d352c46843d78cf
maenodaisuke <developer.daisuke.maeno@gmail.com>
2017-10-20 18:11:16 +0900

    Add example code.

--- arrow-test.js
+++ arrow-test.js
@@ -12,3 +12,13 @@ const arrow2 = (name) => {
 
 arrow1()
 arrow2('hoge')
+
+const str = 'Yes, we can!'
+console.log(str.replace(/[a-z]+/g , (m) => {
+  return m.toUpperCase()
+}))
+
+const array = [10, 1, 3, 2, 8, 8, 88]
+console.log(array.sort((a, b) => {
+  return a - b
+}))
